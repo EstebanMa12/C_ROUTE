37dd5fb14ebf8b06c946590432a47cf64d0c0156
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-12 14:33:32 -0500

    Start with functions

---
+++ IntroductionToC/helloworl.c
@@ -0,0 +1,42 @@
+// Compile: gcc helloworld.c -o helloworld
+// Run: ./helloworld
+#include <stdio.h>
+
+c
+int main()
+{
+    // Print "Hello World!" to the console
+    printf("Hello World!\n");
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
+
+// C es un lenguaje de programación imperativo 
+// Es decir, cada instrucción que se le da hay que definirla muy bien
+// C es un lenguaje de programación estructurado
+// Es decir, se divide en bloques de código que se pueden reutilizar
+
+// *Asignaciones : Guarda un valor en la memoria temporal 
+
+// *Condicionales: A partir de una condición se ejecuta unas instrucciones o no 
+
+// *Bucles: Se ejecuta un bloque de código varias veces
+
+// Llamadas: El flujo de instrucciones pasa a otra zona del código
+
+// PR0GRAMACIÓN PROCEDIMENTAL: 
+//*Funciones: Bloque de código que se puede reutilizar, tiene entradas y salidas
+
+// Funcion main
+// TIPOS DE DATOS :
+// *Enteros: int 100   255
+// *Reales: float, double   10.34 11.21
+// *Caracteres: char 'A' 'B'
+// *Booleanos: bool
+// *Cadenas de caracteres: char*   'HOLA MUNDO'
+// *Void: Nada 
+// *Punteros: int*, float*, char*, bool*, void*
+
+
+
--- helloworl.c
+++
@@ -1,9 +0,0 @@
-// Compile: gcc helloworld.c -o helloworld
-// Run: ./helloworld
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
-}
