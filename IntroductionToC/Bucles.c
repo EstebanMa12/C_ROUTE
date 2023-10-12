29eeccb5c6b02939fe7711fce203fd4e56f9a4b3
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-12 16:28:04 -0500

    Working with loops/ For, while and do while

--- IntroductionToC/Bucles.c
+++ IntroductionToC/Bucles.c
@@ -23,5 +23,34 @@
 // }
 // while (condition);
 int main(){
-  
+    printf("Bucle while:\n");
+    bucle_while(10);
+    printf("Bucle for:\n");
+    bucle_for(10);
+    printf("Bucle do while:\n");
+    bucle_do_while(10);
+    
+    return 0;
+}
+
+void bucle_while(int number){
+  int i = 0;
+    while ( i < number){
+        printf("%d\n", i);
+        i++;
+    }
+}
+
+void bucle_for(int number){
+    for (int i = 0; i < number; i++){
+        printf("%d\n", i);
+    }
+}
+
+void bucle_do_while(int number){
+    int i = 0;
+    do {
+        printf("%d\n", i);
+        i++;
+    } while (i < number);
 }
