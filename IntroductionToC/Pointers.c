ccf437ca9f213176afb45f5a5344b7e4634a7d2c
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-17 15:26:57 -0500

    The following changes were made:
    * Added a function called jugar that takes an integer pointer as an argument.
    * In the jugar function, the value of the pointer is dereferenced and assigned to a variable called y.
    * The value of y is then calculated and assigned back to the pointer.
    * The value of the pointer is then printed.
    * In the main function, a pointer to the num variable is created and the jugar function is called with the pointer as an argument.

--- IntroductionToC/Pointers.c
+++ IntroductionToC/Pointers.c
@@ -10,35 +10,6 @@
 // Un puntero se puede asignar a una variable de tipo char.
 
 
-int main() {
-  // Declare an integer variable named num and initialize it to 25
-  int num = 25;
-
-  // Declare an integer pointer named ptr and initialize it to point to num
-  int *ptr = &num;
-
-  // Print the value of num
-  printf("Value of num: %d\n", num);
-
-  // Print the address of num
-  printf("Address of num: %p\n", &num);
-
-  // Print the value of ptr
-  printf("Value of ptr: %p\n", ptr);
-
-  // Print the value that ptr is pointing to
-  printf("Value of *ptr: %d\n", *ptr);
-
-  printf("Jugando con el número: \n");
-  jugar(&num);
-  printf("Value of num: %d\n", num);
-  
-  // La direccion de memoria sigue siendo la misma
-
-  printf("Address of num: %p\n", &num);
-
-  return 0;
-}
 void jugar(int* n){
   // Cuando ponemos el asterisco a la izquierda de la variable estamos recuperando el valor que hay en el puntero 
 
@@ -56,4 +27,39 @@ void jugar(int* n){
   // Se imprime el valor del puntero
   printf("%d\n", y);
 
+}
+
+
+int main() {
+  // Declare an integer variable named num and initialize it to 25
+  int num = 25;
+
+  // Declare an integer pointer named ptr and initialize it to point to num
+  int *ptr; 
+  ptr = &num;
+
+  // Print the value of num
+  printf("Value of num: %d\n", num);
+
+  // Print the address of num
+  printf("Address of &num: %p\n", &num);
+
+  // Print the value of ptr
+  printf("Value of ptr: %p\n", ptr);
+
+  // Print the address of ptr itself
+  printf("Address of &ptr: %p\n", &ptr);
+
+  // Print the value that ptr is pointing to
+  printf("Value that *ptr ins pointing to: %d\n", *ptr);
+
+  printf("Jugando con el número: \n");
+  jugar(&num);
+  printf("Value of num: %d\n", num);
+  
+  // La direccion de memoria sigue siendo la misma
+
+  printf("Address of &num: %p\n", &num);
+
+  return 0;
 }
