918ae441f7aa9a1b461c67ac015e03f98049da18
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-18 18:55:50 -0500

    Introducing to bits manipulation

---
+++ IntroductionToC/bitsManipulation/main.c
@@ -0,0 +1,8 @@
+// Bit manipulation in c language:
+// C dispone de operadores booleanos que permiten transformar los bits que componen un valor numérico.
+//
+// Los operadores booleanos son:
+//
+
+// 1. Bitwise AND operator &
+// 2. Bitwise OR operator |
