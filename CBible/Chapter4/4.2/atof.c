a113e3096af11ed913ecc84a6507db0f35e9df57
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-22 22:47:39 -0500

    ``` Files a/memoryManagement/cast and b/memoryManagement/cast differ in their binary contents.

---
+++ CBible/Chapter4/4.2/atof.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+
+double atof(char s[]){
+    double val, power;
+    int i, sign;
+
+    for(i = 0; isspace(s[i]); i++) // skip white space
+        ;
+    sign = (s[i] == '-') ? -1 : 1; // get sign
+    if(s[i] == '+' || s[i] == '-') // skip sign
+        i++;
+    for(val = 0.0; isdigit(s[i]); i++) // get integer part
+        val = 10.0 * val + (s[i] - '0');
+    if(s[i] == '.') // skip decimal point
+        i++;
+    for(power = 1.0; isdigit(s[i]); i++){ // get fractional part
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power;
+}
