632b417bef9e3e46af2f5c0e61a80903b3ef7123
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-24 11:38:17 -0500

    Fixed memory leak in `duplicate` function by allocating memory for the `name` field.

--- Exercises/dinamicMemory/llamadasAlSistema/bluetooth.c
+++ Exercises/dinamicMemory/llamadasAlSistema/bluetooth.c
@@ -4,11 +4,11 @@
 
 #define MAC_LENGTH 6
 
-struct bluetooth_info 
+struct bluetooth_info
 {
-    char *name;
-    unsigned int strength;
-    char mac[MAC_LENGTH];
+  char *name;
+  unsigned int strength;
+  char mac[MAC_LENGTH];
 };
 void print_structure(struct bluetooth_info *info);
 
@@ -19,59 +19,57 @@ struct bluetooth_info *duplicate(struct bluetooth_info *);
 
 // The use of a pointer to a struct bluetooth_info allows the function to modify the original object or create a new object without having to copy the entire object. This can be more efficient than copying the entire object, especially if the object is large.
 
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-    struct bluetooth_info *info_ptr;
-    int i;
+  struct bluetooth_info *info_ptr;
+  int i;
+  info_ptr = &info;
+  info_ptr->name = 'My phone';
+  info_ptr->strength = 100;
+  for (i = 0; i < MAC_LENGTH; i++)
+  {
+    info_ptr->mac[i] = 10;
+  }
+  new_info = duplicate(info_ptr);
+  print_structure(new_info);
+  free(new_info);
 
-    info_ptr = &info;
-    info_ptr->name = "My phone";
-    info_ptr->strength = 100;
-    for (i = 0; i < MAC_LENGTH; i++) 
-    {
-      info_ptr->mac[i] = 10;
-    }
-    new_info = duplicate(info_ptr);
-    print_structure(new_info);
-    free(new_info);
-
-    return 0;
+  return 0;
 }
-
-
 // Function to duplicate bluetooth_info struct
 struct bluetooth_info *duplicate(struct bluetooth_info *src_ptr)
 {
-    // Create a new bluetooth_info struct
-    struct bluetooth_info *result;
-    int i;
+  // Create a new bluetooth_info struct
+  struct bluetooth_info *result;
+  int i;
 
-    // The function first allocates memory for a new struct bluetooth_info object using the malloc() function. The size of the object is determined using the sizeof() operator, which returns the size of the struct bluetooth_info type in bytes.
-    result = (struct bluetooth_info *)malloc(sizeof(struct bluetooth_info));
+  // The function first allocates memory for a new struct bluetooth_info object using the malloc() function. The size of the object is determined using the sizeof() operator, which returns the size of the struct bluetooth_info type in bytes.
+  result = (struct bluetooth_info *)malloc(sizeof(struct bluetooth_info));
 
-    // Copy the name from the source struct. This is done by first allocating memory for the name using the malloc() function and then copying the contents of the source name to the new name using the strcpy() function.
-    result->name = (char *)malloc(strlen(src_ptr->name) + 1);
-    strcpy(result->name, src_ptr->name);
+  // Copy the name from the source struct. This is done by first allocating memory for the name using the malloc() function and then copying the contents of the source name to the new name using the strcpy() function.
+  result->name = (char *)malloc(strlen(src_ptr->name) + 1);
+  strcpy(result->name, src_ptr->name);
 
-    // Copy the strength from the source struct
-    result->strength = src_ptr->strength;
+  // Copy the strength from the source struct
+  result->strength = src_ptr->strength;
 
-    // Copy the MAC address from the source struct
-    for (i = 0; i < MAC_LENGTH; i++)
-    {
-      result->mac[i] = src_ptr->mac[i];
-    }
-
-    // Return the duplicated struct
-    return result;
-}
-void print_structure(struct bluetooth_info *info){
-  printf("Name: %s\n", info -> name);
-  printf("Strength: %d\n", info -> strength);
-  printf("MAC: ");
-  for (int i = 0; i < MAC_LENGTH; i++) 
+  // Copy the MAC address from the source struct
+  for (i = 0; i < MAC_LENGTH; i++)
   {
-    printf("%d ", info -> mac[i]);
+    result->mac[i] = src_ptr->mac[i];
+  }
+
+  // Return the duplicated struct
+  return result;
+}
+void print_structure(struct bluetooth_info *info)
+{
+  printf("Name: %s\n", info->name);
+  printf("Strength: %d\n", info->strength);
+  printf("MAC: ");
+  for (int i = 0; i < MAC_LENGTH; i++)
+  {
+    printf("%d ", info->mac[i]);
   }
   printf("\n");
 }
