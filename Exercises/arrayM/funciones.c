f751a8071d10a43826041fd75989e039ba5b0a31
EstebanMa12 <desteban.maya@udea.edu.co>
2023-10-31 08:10:28 -0500

    ``` Add functions to insert and delete elements in an array, and update an element in an array. ``` (generated by useblackbox.ai )

--- Exercises/arrayM/funciones.c
+++ Exercises/arrayM/funciones.c
@@ -83,3 +83,63 @@ int buscar2(int *array, int *prt_size_array, int valor ){
   return -1;
   
 }
+// Insertar con realloc
+void insertar_inicio(int *array, int *prt_size, int valor){
+  int i;
+  *prt_size = *prt_size + 1;
+  array = (int *)realloc(array, *prt_size * sizeof(int));  
+  for(i=*prt_size; i>0; i--){
+    array[i] = array[i-1];
+  }
+  array[0] = valor;
+}
+
+void insertar_fin(int *array, int *prt_size, int valor){
+  *prt_size = *prt_size + 1;
+  array = (int *)realloc(array, *prt_size * sizeof(int));  
+  array[*prt_size] = valor;
+}
+
+void insertar_posicion(int *array, int *prt_size, int valor, int posicion){
+  int i;
+  *prt_size = *prt_size + 1;
+  array = (int *)realloc(array, *prt_size * sizeof(int));  
+  for(i=*prt_size; i>posicion; i--){
+    array[i] = array[i-1];
+  }
+  array[posicion] = valor;
+}
+
+void eliminar_inicio(int *array, int *prt_size_array){
+  int i;
+  for(i=0; i<*prt_size_array; i++){
+    array[i]= array[i+1];
+  }
+  *prt_size_array = *prt_size_array - 1;
+  array = (int *)realloc(array, *prt_size_array * sizeof(int));  
+}
+
+void eliminar_final(int *array, int *prt_size_array){
+  *prt_size_array = *prt_size_array - 1;
+  array = (int *)realloc(array, *prt_size_array * sizeof(int));  
+}
+
+void eliminar_en_i(int *array, int *prt_size_array, int posicion){
+  int i ;
+  for(i=posicion; i<*prt_size_array; i++){
+    array[i]= array[i+1];
+  }
+  *prt_size_array = *prt_size_array - 1;
+  array = (int *)realloc(array, *prt_size_array * sizeof(int));  
+}
+
+void actualizar(int *array, int *prt_size_array, int posicion, int valor){
+  array[posicion] = valor;
+}
+void imprimir(int *array, int *prt_size_array){
+  int i;
+  for(i=0; i<*prt_size_array; i++){
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+}
